97d4cacb81e754eee4983bfbcd39eff617866eb9
AnasJMirza <ajmhimself@gmail.com>
2022-08-05 23:59:15 +0500

    make it better

--- tinder-backend/schema.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-const schema = new Schema({
-    name:  String,
-    imgURL: String,
-  });
-
-export default mongoose.model("Cards", schema)
